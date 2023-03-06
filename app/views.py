7f88082fb3926de780f0d351a2076947a8054aff
sairanjansahu <sairanjansahu2000@gmail.com>
2023-03-06 23:24:47 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def Raj(request):
+    return HttpResponse('RAJ IS THE BEST ALL- ROUNDER IN OUR STATE')
+
+def Sunanda(request):
+    return HttpResponse('i love u RAJ')
